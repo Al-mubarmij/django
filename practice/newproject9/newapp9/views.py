e98d04a4bb509b32676d5adac4d790a59e98177b
Al-mubarmij <42393721+Al-mubarmij@users.noreply.github.com>
2024-05-23 00:21:31 +0400

    finished the multiple views lap successfully!

---
+++ practice/newproject9/newapp9/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse("Welcome to Little Lemon")
+
+def about(request):
+    return HttpResponse("About us")
+
+def menu(request):
+    return HttpResponse("menu")
+
+
+def book(request):
+    return HttpResponse("Make a booking")
+
