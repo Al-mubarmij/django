8a3eb0f52e31f1af34aa67c367cdabe8be043b44
Al-mubarmij <42393721+Al-mubarmij@users.noreply.github.com>
2024-05-25 05:06:22 +0400

    adding two models exercise

--- practice/modelsproject4/modelsapp4/admin.py
+++ practice/modelsproject4/modelsapp4/admin.py
@@ -2,3 +2,5 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 admin.site.register(models.Drinks)
+admin.site.register(models.Menu)
+admin.site.register(models.MenuCategory)
--- practice/modelsproject4/modelsapp4/models.py
+++ practice/modelsproject4/modelsapp4/models.py
@@ -5,3 +5,11 @@ class Drinks(models.Model):
     drink = models.CharField(max_length=200, default='lemon')
     price = models.IntegerField(default=0)
     
+
+class MenuCategory(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
+class Menu(models.Model):
+    menu_item = models.CharField(max_length=200)
+    price = models.IntegerField()
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT, default=None)
